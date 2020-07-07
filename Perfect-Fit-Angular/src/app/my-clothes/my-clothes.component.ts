18a2b03de30e4c2cbd63fb78f36c48165acc8f47
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2020-07-07 18:10:21 -0400

    Add files via upload

--- Perfect-Fit-Angular/src/app/my-clothes/my-clothes.component.ts
+++ Perfect-Fit-Angular/src/app/my-clothes/my-clothes.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ClothingCardComponent } from '../clothing-card/clothing-card.component';
 import { ClothesDbService } from '../clothes-db.service';
+import { EventFlagsService } from '../event-flags.service';
 
 @Component({
 	selector: 'app-my-clothes',
 	templateUrl: './my-clothes.component.html',
 	styleUrls: ['./my-clothes.component.css']
 })
-export class MyClothesComponent implements OnInit
+export class MyClothesComponent
 {
 	myClothes: Array<ClothingCardComponent> = [];
 	
-	constructor(private clothesService: ClothesDbService) { }
+	constructor(private clothesService: ClothesDbService, private eventFlagsService: EventFlagsService) { }
 
 	ngOnInit(): void
 	{
 		console.log("oninit");
 		this.updateClothes();
+	}
+	
+	ngDoCheck()
+	{
+		if(this.eventFlagsService.updateMyClothesFlag == true)
+		{
+			this.updateClothes();
+			this.eventFlagsService.updateMyClothesFlag = false;
+		}
 	}
 	
 	updateClothes(): void
