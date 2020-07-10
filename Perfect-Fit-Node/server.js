413a6d6727fcd8b5aadde7a153b756c10f6674e8
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2020-07-10 18:22:35 -0400

    Add files via upload

--- Perfect-Fit-Node/server.js
+++ Perfect-Fit-Node/server.js
@@ -19,17 +19,29 @@ con.connect(function(err)
 	}
 });
 
-//Returns a clothing object.
-function getRandomArticle()
+//Returns a Promise that contains the result of a mySql query.
+//Right now it doesn't support parameters...
+//Also, I only use it once in one case to await a query... must be a better solution
+function mySQLquery(str)
 {
-	//Get random row from myclothesCopy
-	/// NEED TO HANDLE ERROR IF myclothesCopy DOES NOT EXIST
-	sql = 	'SELECT * FROM myclothesCopy AS r1'
-			+ ' JOIN(SELECT CEIL(RAND() *(SELECT MAX(id) FROM myclothesCopy)) AS id) AS r2'
-			+ ' WHERE r1.id >= r2.id ORDER BY r1.id ASC LIMIT 1';
-			
+	//Potential for buffer overflow? lol
 	return new Promise((resolve, reject) => {
-		var clothingRow = {};
+		con.query(str, function (err, result, fields) {
+			if (err) 
+			{
+				console.log(err);
+				return reject(err);
+			}
+			
+			return resolve(result);
+		});
+	});
+}
+
+function getMatchingArticles(article1, article2)
+{
+	return new Promise((resolve, reject) => {
+		var sql = 'SELECT * FROM myclothesCopy WHERE type = "PANTS"';
 		con.query(sql, function (err, result, fields) {
 			if (err) 
 			{
@@ -37,31 +49,33 @@ function getRandomArticle()
 				return reject(err);
 			}
 			
-			console.log("	- random article get");
-			
-			var ssqqll = "SELECT * FROM myclothesCopy";
-			con.query(ssqqll, function (err, result, fields) {
-				if (err) throw err;
-				console.log('> ', result);
-			});
-			
-			
-			clothingRow = {id: result[0].id, article: result[0].article, color: result[0].color, material: result[0].material};
-			return resolve(clothingRow);
+			return resolve(result);
 		});
 	});
 }
 
-//Returns an array of clothing objects, guaranteed to not contain selectedArticle.
-async function getRandomArticles(amount, selectedArticle)
+//Returns an array of arrays of clothing objects that match with selectedArticle
+async function getOutfits(amount, selectedArticle)
 {
-	var clothingArr = [];
+	if(amount <= 0)
+	{
+		return [];
+	}
+	
+	var outfits = [];
 	
 	//Create copy of myclothes (can't use a temp table because getRandomArticle uses a query that accesses a table twice)
 	var sql = 'CREATE TABLE IF NOT EXISTS myclothesCopy AS SELECT * FROM myclothes';
 	con.query(sql, function (err, result, fields) {
 		if (err) throw err;
 		console.log('- Temporary table exists');
+	});
+	
+	sql = 'SELECT * FROM myclothesCopy WHERE type = "SHIRT"';
+	//var type = (selectedArticle.type = 'JACKET') ? 'SHIRT' : 'JACKET';
+	con.query(sql, function (err, result, fields) {
+		if (err) throw err;
+		console.log('$', result);
 	});
 	
 	//Remove selectedArticle from myclothesCopy
@@ -79,42 +93,42 @@ async function getRandomArticles(amount, selectedArticle)
 				}
 	);
 	
-	for(var i = 0; i < amount; ++i)
-	{
-		var clothingRow = await getRandomArticle().then((clothingRow) => { return clothingRow; }).catch((err) => { return {}; });
-		
-		//Since the id's in myclothes are auto-generated, they don't get copied over correctly to myclothesCopy.
-		//May as well just use the article/color/material.
-		var sql = 'DELETE FROM myclothesCopy WHERE article = ? AND color = ? AND material = ?';
-		con.query(sql, [clothingRow.article, clothingRow.color, clothingRow.material], function (err, result, fields) {
-			if (err) throw err;
-			console.log('	- ', clothingRow.article, ' deleted from myclothesCopy');
-		});
+	//Retrieve shirts from myclothesCopy
+	sql = 'SELECT * FROM myclothesCopy WHERE type = "SHIRT"';
+	var shirts = await mySQLquery(sql).then((result) => {return result;}).catch((error) => {return {}; });
 	
-		clothingArr.push( {article: clothingRow.article, color: clothingRow.color, material: clothingRow.material} );
+	for(var s in shirts)
+	{
+		if(true /*jacket matches with shirt*/)
+		{
+			var matchingArticles = await getMatchingArticles(selectedArticle, shirts[s]).then((result) => {return result;}).catch((error) => {return {}; });;
+			
+			for(var p in matchingArticles)
+			{
+				outfits.push([shirts[s], matchingArticles[p]]);
+				if(outfits.length == amount)
+				{
+					//Delete myclothesCopy 
+					sql = 'DROP TABLE myclothesCopy';
+					con.query(sql, function (err, result, fields) {
+						if (err) throw err;
+						console.log('Temporary table dropped');
+					});
+					
+					console.log('~ ' + outfits);
+					
+					return outfits;
+				}
+			}
+		}
 	}
 	
-	//Delete myclothesCopy
+	//Delete myclothesCopy 
 	sql = 'DROP TABLE myclothesCopy';
 	con.query(sql, function (err, result, fields) {
 		if (err) throw err;
-		console.log('- Temporary table dropped');
+		console.log('Temporary table dropped');
 	});
-			
-	return new Promise((resolve, reject) => {
-		return resolve(clothingArr);
-	});
-}
-
-//Returns an array of arrays of clothing objects that match with selectedArticle
-async function getOutfits(selectedArticle)
-{
-	//Prepare for this to return a promise...
-	return [
-		await getRandomArticles(3, selectedArticle).then((clothingArr) => { return clothingArr; }).catch((err) => { return {}; }),
-		await getRandomArticles(3, selectedArticle).then((clothingArr) => { return clothingArr; }).catch((err) => { return {}; }),
-		await getRandomArticles(3, selectedArticle).then((clothingArr) => { return clothingArr; }).catch((err) => { return {}; })
-	];
 }
 	
 
@@ -125,7 +139,7 @@ app.listen(3000, function ()
 {
 	console.log('Example app listening on port 3000!');
 	
-	var sql = 'CREATE TABLE IF NOT EXISTS myclothes (id int AUTO_INCREMENT, article VARCHAR(255), color VARCHAR(255), material VARCHAR(255), PRIMARY KEY (id))';
+	var sql = 'CREATE TABLE IF NOT EXISTS myclothes (id int AUTO_INCREMENT, article VARCHAR(255), color VARCHAR(255), material VARCHAR(255), type VARCHAR(255), PRIMARY KEY (id))';
 	con.query(sql, function (err, result, fields) {
 		if (err) throw err;
 		console.log('Table exists');
@@ -146,13 +160,15 @@ app.post('/api/addClothes', function (req, res)
 	var article = req.body.article.toUpperCase();
 	var color = req.body.color.toUpperCase();
 	var material = req.body.material.toUpperCase();
+	var type = req.body.type.toUpperCase();
 	
-	sql = 'INSERT INTO myclothes (article, color, material) VALUES (?, ?, ?)';
+	sql = 'INSERT INTO myclothes (article, color, material, type) VALUES (?, ?, ?, ?)';
 	con.query(	sql, 	
 				[ 	
 					article,
 					color,
-					material
+					material,
+					type
 				],						
 				function (err, result, fields) 
 				{
@@ -190,7 +206,8 @@ app.post('/api/removeClothes', function(req, res)
 
 app.post('/api/getOutfits', async function(req, res)
 {
-	var outfitsArray = await getOutfits(req.body);
+	var outfitsArray = await getOutfits(3, req.body);
+	//console.log('~ ' + outfitsArray);
 	
 	res.send(outfitsArray);
 	return;
